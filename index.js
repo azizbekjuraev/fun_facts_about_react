e437b404b34c1c0ba7c653bc5cebd06c6498a782
Abdulaziz <ajuraev98@mail.ru>
2022-12-08 17:23:20 +0500

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"));
+// let divRoot = document.getElementById("root");
+
+// let pEl = document.createElement("p");
+// pEl.id = "header";
+// pEl.innerHTML = `Hello React!`;
+// divRoot.appendChild(pEl);
+// console.log(pEl);
+
+function MainContent() {
+  return (
+    <div className="main">
+      <h1 id="h1">I am learning react</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<MainContent />, document.getElementById("root"));
+
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
