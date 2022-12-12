592696c6c0a517bd93ead9551dae25392cbd3ce7
AZIZBEKJURAEV <ajuraev98@mail.ru>
2022-12-12 09:01:11 +0500

    fixed some bugs

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// const page = (
-//   <div id="main-page">
-//     <img id="react-logo" src="./react-logo.png" />
-//     <h1>Fun facts about react</h1>
-//     <ul>
-//       <li>Was first released in 2013</li>
-//       <li>Was first released in 2014</li>
-//       <li>Was first released in 2015</li>
-//       <li>Was first released in 2016</li>
-//       <li>Was first released in 2017</li>
-//     </ul>
-//   </div>
-// );
+function React() {
+  return (
+    <div>
+      <ul>
+        <li>Its fast</li>
+      </ul>
+    </div>
+  );
+}
 
-// ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<React />, document.getElementById("root"));
