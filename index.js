f4e51dca3a539767f827d5c249fb888df34abca2
AZIZBEKJURAEV <ajuraev98@mail.ru>
2022-12-12 09:28:14 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function React() {
+function Page() {
   return (
     <div>
-      <ul>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width={"60px"} />
+        </nav>
+      </header>
+      <h1>Why I like React?</h1>
+      <ol>
         <li>Its fast</li>
-      </ul>
+        <li>Its Maintained by Facebook</li>
+        <li>It runs many apps in the world</li>
+        <li>User friendly</li>
+      </ol>
+      <footer>
+        <small>©️ 2022 JURAEV development. All rights reserved</small>
+      </footer>
     </div>
   );
 }
 
-ReactDOM.render(<React />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
